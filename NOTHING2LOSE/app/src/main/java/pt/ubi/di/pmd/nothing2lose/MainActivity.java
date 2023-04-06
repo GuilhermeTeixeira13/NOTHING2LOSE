1b578f7af8f0882cad79cf65d06eac1a902aad3f
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-04-06 18:15:06 +0100

    Acess to AWS Postgres DB working.

--- NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/MainActivity.java
+++ NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/MainActivity.java
@@ -1,14 +1,23 @@
 package pt.ubi.di.pmd.nothing2lose;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         return (max - min) * prize + min;
     }
 
-    public ArrayList<Award> createAwards(double lambda, double minPrize, double maxPrize){
+    public ArrayList<Award> createAwards(double lambda, double minPrize, double maxPrize) {
         ArrayList<Award> awards = new ArrayList<>();
         ArrayList<Integer> prizes = new ArrayList<>();
         int category = 1;
@@ -59,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         Collections.sort(prizes);
 
-        for(int prize : prizes){
+        for (int prize : prizes) {
             Award award = new Award(prize, category);
             awards.add(award);
             category++;
@@ -68,8 +77,58 @@ public class MainActivity extends AppCompatActivity {
         return awards;
     }
 
-    public void AwardAChoosen(View v){
+    public void AwardAChoosen(View v) throws SQLException {
         Log.d("MyApp", "Clicked on A award.");
+
+        new DatabaseQueryTask().execute();
+    }
+
+    private class DatabaseQueryTask extends AsyncTask<Void, Void, List<String>> {
+        private Exception exception;
+
+        protected List<String> doInBackground(Void... voids) {
+            List<String> results = new ArrayList<>();
+
+            String url = "jdbc:postgresql://nothing2lose-db.carkfyqrpaoi.eu-north-1.rds.amazonaws.com:5432/NOTHING2LOSEDB";
+            String username = "postgres";
+            String password = "8iy5df232";
+
+            try {
+                Class.forName("org.postgresql.Driver");
+                Connection conn = DriverManager.getConnection(url, username, password);
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM users");
+
+                while (rs.next()) {
+                    // Process the data retrieved from the database
+                    // For example, you can add the values to a list
+                    String id = String.valueOf(rs.getInt("ID"));
+                    String user = rs.getString("username");
+                    String pass = rs.getString("password");
+                    results.add("ID: " + id + ", Username: " + user + ", Password: " + pass);
+                }
+
+                rs.close();
+                stmt.close();
+                conn.close();
+            } catch (Exception e) {
+                Log.e("MyApp", "Error executing query", e);
+                exception = e;
+            }
+
+            return results;
+        }
+
+        protected void onPostExecute(List<String> results) {
+            if (exception != null) {
+                Toast.makeText(MainActivity.this, "Error executing query: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+            } else {
+                // Update UI with the database query results
+                for (String result : results) {
+                    Log.d("MyApp", result);
+                }
+            }
+        }
     }
 
     public void AwardBChoosen(View v){
