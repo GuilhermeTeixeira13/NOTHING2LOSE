844149eb5df28651a363e3906b2f6f810b497558
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-04-07 22:26:25 +0100

    Check if username is already taken and then register user.

--- NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/RegisterActivity.java
+++ NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/RegisterActivity.java
@@ -2,7 +2,9 @@ package pt.ubi.di.pmd.nothing2lose;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -23,7 +25,6 @@ import com.google.android.material.textfield.TextInputEditText;
 public class RegisterActivity extends AppCompatActivity {
 
     TextInputEditText nicknameEditText;
-    TextInputEditText editTextEmail;
     TextInputEditText editTextPassword;
     TextInputEditText editTextPasswordRepeat;
 
@@ -33,62 +34,92 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register);
 
        nicknameEditText = findViewById(R.id.editTextNickname);
-       editTextEmail = findViewById(R.id.editTextEmail);
        editTextPassword = findViewById(R.id.editTextPassword);
        editTextPasswordRepeat = findViewById(R.id.editTextPasswordRepeat);
     }
 
-    public void onRegisterClicked(View view){
-        Log.d("MyApp", "cliquei no registo");
+    public void onRegisterClicked(View view) {
+        String nickname = nicknameEditText.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String passwordRepeat = editTextPasswordRepeat.getText().toString().trim();
 
-        if(!nicknameEditText.getText().toString().equals("") && !editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("") && !editTextPasswordRepeat.getText().toString().equals("")){
-            if(editTextPassword.getText().toString().equals(editTextPasswordRepeat.getText().toString())){
-                // Passwords match
-                new RegisterActivity.UserRegistration().execute();
-            }
-            else {
-                // Passwords don't match
-                Toast.makeText(RegisterActivity.this, "It seems like the passwords you entered don't match!", Toast.LENGTH_LONG).show();
-            }
+        if (nickname.isEmpty() || password.isEmpty() || passwordRepeat.isEmpty()) {
+            // Empty fields
+            Toast.makeText(RegisterActivity.this, "Hey there! It seems like you left some fields empty!", Toast.LENGTH_LONG).show();
+            return;
         }
-        else {
-            // Empty Textfields
-            Toast.makeText(RegisterActivity.this, "Hey there! It seems like you left some fields empty!" , Toast.LENGTH_LONG).show();
+
+        if (!password.equals(passwordRepeat)) {
+            // Passwords don't match
+            Toast.makeText(RegisterActivity.this, "It seems like the passwords you entered don't match!", Toast.LENGTH_LONG).show();
+            return;
         }
+
+        new CheckIfUserExistsTask().execute(nickname, password);
     }
 
-    public void onLoginLinkClicked(View view){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    private class UserRegistration extends AsyncTask<Void, Void, Void> {
+    private class CheckIfUserExistsTask extends AsyncTask<String, Void, Boolean> {
         private Exception exception;
 
-        protected Void doInBackground(Void... voids) {
-            String url = "jdbc:postgresql://nothing2lose-db.carkfyqrpaoi.eu-north-1.rds.amazonaws.com:5432/NOTHING2LOSEDB";
-            String username = "postgres";
-            String password = "8iy5df232";
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String nickname = params[0];
+            String password = params[1];
 
-            // Passwords match
-            try {
-                Class.forName("org.postgresql.Driver");
-                Connection conn = DriverManager.getConnection(url, username, password);
+            String svurl = "jdbc:postgresql://nothing2lose-db.carkfyqrpaoi.eu-north-1.rds.amazonaws.com:5432/NOTHING2LOSEDB";
+            String svusername = "postgres";
+            String svpassword = "8iy5df232";
 
-                String insertQuery = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-                PreparedStatement pstmt = conn.prepareStatement(insertQuery);
+            try (Connection conn = DriverManager.getConnection(svurl, svusername, svpassword)) {
+                // Check if there is another user with the same username
+                String selectQuery = "SELECT COUNT(*) FROM users WHERE username=?";
+                try (PreparedStatement pstmt = conn.prepareStatement(selectQuery)) {
+                    pstmt.setString(1, nickname);
+                    try (ResultSet rs = pstmt.executeQuery()) {
+                        rs.next();
+                        int count = rs.getInt(1);
+                        return count == 0;
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("MyApp", "Error executing query", e);
+                exception = e;
+                return false;
+            }
+        }
 
-                // set values for the parameters
-                pstmt.setString(1, nicknameEditText.getText().toString());
-                pstmt.setString(2, editTextEmail.getText().toString());
-                pstmt.setString(3, editTextPassword.getText().toString());
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
+                String nickname = nicknameEditText.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                new UserRegistrationTask().execute(nickname, password);
+            } else {
+                Toast.makeText(RegisterActivity.this, "Sorry, the nickname is already taken!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 
-                // execute the insert statement
-                pstmt.executeUpdate();
+    private class UserRegistrationTask extends AsyncTask<String, Void, Void> {
+        private Exception exception;
 
-                pstmt.close();
-                conn.close();
+        @Override
+        protected Void doInBackground(String... params) {
+            String nickname = params[0];
+            String password = params[1];
 
+            String svurl = "jdbc:postgresql://nothing2lose-db.carkfyqrpaoi.eu-north-1.rds.amazonaws.com:5432/NOTHING2LOSEDB";
+            String svusername = "postgres";
+            String svpassword = "8iy5df232";
+
+            try (Connection conn = DriverManager.getConnection(svurl, svusername, svpassword)) {
+                String insertQuery = "INSERT INTO users (username, password) VALUES (?, ?)";
+                try (PreparedStatement pstmt = conn.prepareStatement(insertQuery)) {
+                    pstmt.setString(1, nickname);
+                    pstmt.setString(2, password);
+                    pstmt.executeUpdate();
+                }
+                saveUserInSharedPreferences();
                 goToGamePage();
             } catch (Exception e) {
                 Log.e("MyApp", "Error executing query", e);
@@ -97,9 +128,23 @@ public class RegisterActivity extends AppCompatActivity {
             return null;
         }
     }
+    
+    public void onLoginLinkClicked(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     public void goToGamePage(){
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+    }
+
+    public void saveUserInSharedPreferences(){
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("username", nicknameEditText.getText().toString());
+        editor.putString("password", editTextPassword.getText().toString());
+        editor.commit();
     }
 }
