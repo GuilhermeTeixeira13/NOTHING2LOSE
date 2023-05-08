5a9cd6b58dd6b6978d8824dced3a01870ccae464
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2023-05-08 15:03:53 +0100

    Hmacs calculados.

--- NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/CalculateHMAC.java
+++ NOTHING2LOSE/app/src/main/java/pt/ubi/di/pmd/nothing2lose/CalculateHMAC.java
@@ -6,6 +6,8 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -13,25 +15,37 @@ import java.util.List;
 public class CalculateHMAC {
     Mac[] hmacs = new Mac[4];
 
-    public List<byte[]> claculateHmac(List keys, ArrayList awards) {
-        String[] prizes;
+    public List<byte[]> claculateHmac(List<byte[]> keys, ArrayList<Award> awards) {
         List<byte[]> hmacS = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            prizes = (String[]) awards.get(i);
-            try {
-                hmacs[i] = Mac.getInstance("HmacSHA256");
-                hmacs[i].init((Key) keys.get(i));
-                byte[] hmacBytes = hmacs[i].doFinal(prizes[i].getBytes());
-                hmacS.add(hmacBytes);
 
-                String hmac = Base64.getEncoder().encodeToString(hmacBytes);
-                System.out.println(prizes[i] + ": " + hmac);
-            } catch (NoSuchAlgorithmException e) {
+        for (int i = 0; i < 4 ; i++){
+            Award award = awards.get(i);
+            byte[] key = keys.get(i);
+
+            try {
+                // Create an HMAC instance with the SHA-256 algorithm
+                Mac hmac = Mac.getInstance("HmacSHA256");
+
+                // Create a SecretKeySpec with the current key and algorithm
+                SecretKeySpec keySpec = new SecretKeySpec(key, "HmacSHA256");
+
+                // Initialize the HMAC with the key
+                hmac.init(keySpec);
+
+                // Calculate the HMAC of the award
+                byte[] hmacValue = hmac.doFinal(award.toByteArray());
+
+                // Add the HMAC to the result list
+                hmacS.add(hmacValue);
+
+            } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+                // Handle exceptions here
                 e.printStackTrace();
-            } catch (InvalidKeyException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         return hmacS;
     }
 
